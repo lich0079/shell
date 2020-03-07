8863b014a48057ee2d812d9ad947de8d0e15c219
lich0079 <lich0079@gmail.com>
2020-03-07 22:28:47 +0800

    add cut_tail script

---
+++ cut_tiktik_tail/cut_tail.sh
@@ -0,0 +1,21 @@
+#/bin/sh
+
+endTime=0
+three=3
+dur=0
+
+FILES=/Users/lich/Downloads/*
+
+for f in $FILES
+do
+    # get duration of a video in seconds
+    endTime=`ffprobe -i ${f} -show_format -v quiet | sed -n "s/duration=//p"`
+    # float to int
+    endTimeInt=${endTime%.*}
+    # minus 3 seconds which for tiktik brand
+    dur=$(($endTimeInt - $three))
+    echo $dur
+    # generate the video with out tiktik part
+    CMD=`ffmpeg  -ss -00:00   -to  ${dur}  -i  ${f}  ${f}__.mp4`
+
+done

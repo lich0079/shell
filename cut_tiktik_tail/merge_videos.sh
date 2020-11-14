4a0fae059d79fde9b396bb08deb2780d653f7616
lich0079 <lich0079@gmail.com>
2020-11-14 08:39:14 +0800

    add merge videos

---
+++ cut_tiktik_tail/merge_videos.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+PATH=/Users/lich/Downloads/tmp/
+
+FILES=$PATH*
+
+
+CMD=`/bin/rm "${PATH}"mylist.txt`
+
+for f in $FILES
+do
+    echo "file ${f}" >> ${PATH}mylist.txt
+done
+
+CMD=`/usr/local/bin/ffmpeg  -f concat -safe 0 -i "${PATH}"mylist.txt -c copy "${PATH}"output.mkv`
